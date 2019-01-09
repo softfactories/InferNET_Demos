d3367e00f362ef287cb0b0702958e5f73a4aba81
softfactories <ceo@soft-fact.de>
2019-01-09 12:08:29 +0100

    Upate - Infer.NET packages and Programm-logic

--- InferNET_Examples/TwoCoins/Program.cs
+++ InferNET_Examples/TwoCoins/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.ML.Probabilistic.Models;
 
-namespace TwoCoins
+namespace sf.infernet.demos
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Variable<bool> ersteMünze = Variable.Bernoulli(0.5);
+            Variable<bool> zweiteMünze = Variable.Bernoulli(0.5);
+            Variable<bool> beideMünzen = ersteMünze & zweiteMünze;
+
+            InferenceEngine engine = new InferenceEngine();
+
+            Console.WriteLine("Die Wahrscheinlichkeit - beide Münzen zeigen Köpfe: "
+                + engine.Infer(beideMünzen));
         }
     }
 }
