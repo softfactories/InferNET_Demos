ec345a292f37bbc85ae9f27a724bd86e7f091068
softfactories <ceo@soft-fact.de>
2019-01-22 17:49:50 +0100

    Experiment-2 ported to TwoCoinsExperiment-Project (.NET Cor Console)

--- InferNET_Examples/TwoCoins/Program.cs
+++ InferNET_Examples/TwoCoins/Program.cs
@@ -8,12 +8,7 @@ namespace sf.infernet.demos
     {
         static void Main(string[] args)
         {
-#if RUN_EXP_1
             Experiment_1();
-#endif
-#if RUN_EXP_2
-            Experiment_2();
-#endif
         }
 
         private static void Experiment_1()
@@ -31,43 +26,8 @@ namespace sf.infernet.demos
             double beideMünzenZeigenKöpfe = ergebnis.GetProbTrue();
 
 
-            Console.WriteLine("Die Wahrscheinlichkeit - beide Münzen zeigen Köpfe: {0}", beideMünzenZeigenKöpfe);
-        }
-
-        private static void Experiment_2()
-        {
-            // PM erstellen
-            Variable<bool> ersteMünzeKopf = Variable.Bernoulli(0.5);
-            Variable<bool> zweiteMünzeKopf = Variable.Bernoulli(0.5);
-            Variable<bool> beideMünzenKopf = ersteMünzeKopf & zweiteMünzeKopf;
-            
-            // Inferenz-Engine (IE) erstellen
-            InferenceEngine engine = new InferenceEngine();
-#if SHOW_MODEL
-            engine.ShowFactorGraph = true; // PM visualisieren
-#endif
-
-            // 1. Inferenz ausführen - beide Münzen zeigen Köpfe
-            var ergebnis1 = engine.Infer<Bernoulli>(beideMünzenKopf);
-            double beideMünzenZeigenKöpfe = ergebnis1.GetProbTrue();
-            var ergebnis2 = engine.Infer<Bernoulli>(ersteMünzeKopf);
-            double ersteMünzeZeigtKopf = ergebnis2.GetProbTrue();
-
-            Console.WriteLine("Prior: Die Wahrscheinlichkeit - beide Münzen zeigen Köpfe: {0}", beideMünzenZeigenKöpfe);
-            Console.WriteLine("Prior: Die Wahrscheinlichkeit - 1. Münze zeigt Kopf: {0}", ersteMünzeZeigtKopf);
-
-            // Beobachtung - beide Münzen zeigen Köpfe nicht
-            beideMünzenKopf.ObservedValue = false;
-            Console.WriteLine("\nBeobachtung: beide Münzen zeigen Köpfe = {0}\n", beideMünzenKopf.ObservedValue);
-
-            // 2. Inferenz ausführen - beide Münzen zeigen Köpfe
-            var ergebnis3 = engine.Infer<Bernoulli>(beideMünzenKopf);
-            beideMünzenZeigenKöpfe = ergebnis3.GetProbTrue();
-            var ergebnis4 = engine.Infer<Bernoulli>(ersteMünzeKopf);
-            ersteMünzeZeigtKopf = ergebnis4.GetProbTrue();
-
-            Console.WriteLine("Posterior: Die Wahrscheinlichkeit - beide Münzen zeigen Köpfe: {0}", beideMünzenZeigenKöpfe);
-            Console.WriteLine("Posterior: Die Wahrscheinlichkeit - 1. Münze zeigt Kopf: {0}", ersteMünzeZeigtKopf);
+            Console.WriteLine("Die Wahrscheinlichkeit - beide Münzen " +
+                "zeigen Köpfe: {0}", beideMünzenZeigenKöpfe);
         }
     }
 }
---
+++ InferNET_Examples/TwoCoinsExperiment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Distributions;
+
+namespace sf.infernet.demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Experiment_2();
+        }
+
+        private static void Experiment_2()
+        {
+            // PM erstellen
+            Variable<bool> ersteMünzeKopf = Variable.Bernoulli(0.5);
+            Variable<bool> zweiteMünzeKopf = Variable.Bernoulli(0.5);
+            Variable<bool> beideMünzenKopf = ersteMünzeKopf & zweiteMünzeKopf;
+
+            // Inferenz-Engine (IE) erstellen
+            InferenceEngine engine = new InferenceEngine();
+#if SHOW_MODEL
+            engine.ShowFactorGraph = true; // PM visualisieren
+#endif
+
+            // 1. Inferenz ausführen - beide Münzen zeigen Köpfe
+            var ergebnis1 = engine.Infer<Bernoulli>(beideMünzenKopf);
+            double beideMünzenZeigenKöpfe = ergebnis1.GetProbTrue();
+            var ergebnis2 = engine.Infer<Bernoulli>(ersteMünzeKopf);
+            double ersteMünzeZeigtKopf = ergebnis2.GetProbTrue();
+
+            Console.WriteLine("Prior: Beide Münzen zeigen Köpfe: {0}"
+                , beideMünzenZeigenKöpfe);
+            Console.WriteLine("Prior: 1. Münze zeigt Kopf: {0}"
+                , ersteMünzeZeigtKopf);
+
+            // Beobachtung - beide Münzen zeigen Köpfe nicht
+            beideMünzenKopf.ObservedValue = false;
+            Console.WriteLine("\nBeobachtung: beide Münzen zeigen Köpfe = {0}\n"
+                , beideMünzenKopf.ObservedValue);
+
+            // 2. Inferenz ausführen - beide Münzen zeigen Köpfe
+            var ergebnis3 = engine.Infer<Bernoulli>(beideMünzenKopf);
+            beideMünzenZeigenKöpfe = ergebnis3.GetProbTrue();
+            var ergebnis4 = engine.Infer<Bernoulli>(ersteMünzeKopf);
+            ersteMünzeZeigtKopf = ergebnis4.GetProbTrue();
+
+            Console.WriteLine("Posterior: P(beide Münzen zeigen Köpfe)={0}", 
+                beideMünzenZeigenKöpfe);
+            Console.WriteLine("Posterior: P(1. Münze zeigt Kopf)={0}", 
+                ersteMünzeZeigtKopf);
+        }
+    }
+}
